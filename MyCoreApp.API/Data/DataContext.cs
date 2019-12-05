229330cc4178b8b4a67d8a225aa998d05fcd878b
Your NameTaimoor <taimoor_aiz@hotmail.com>
2019-12-05 20:06:08 +0000

    Initial Commit

---
+++ MyCoreApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MyCoreApp.API.Models;
+
+namespace MyCoreApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
